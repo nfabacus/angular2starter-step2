34a14611e9f7bbb0559c419354457b496376b10c
nobuyuki.fujioka <nobuyuki.fujioka@ecotricity.co.uk>
2017-02-27 22:12:24 +0000

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'bs-app',
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+  pageTitle: string = "Dev Book Store"
+}
